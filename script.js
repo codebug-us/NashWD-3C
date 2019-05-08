27946dc5fd4d2da97f7287764f718f48f3851643
Ford Chope <douglas.f.chope@vanderbilt.edu>
2019-05-08 11:38:32 -0500

    rock paper scissors

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ document.addEventListener('DOMContentLoaded', function() {
   var options = ["rock", "paper", "scissors"];
   var computerChoice = options[Math.floor(Math.random() * options.length)];
   // Your prompt() goes below
-
+var userChoice = prompt ("rock, paper, or scissors fool");
+console.log("The user chooses: " + userChoice);
 
 
 
@@ -12,12 +13,30 @@ document.addEventListener('DOMContentLoaded', function() {
 
   console.log("The computer chooses: " + computerChoice);
   // Your declareWinner function goes below
+function declareWinner(userChoice,computerChoice){
 
-
-
-
-
-
-
+	if (userChoice == "rock" && computerChoice == "rock"){
+		return "TIE";
+	} else  if (userChoice == "rock" && computerChoice == "scissors"){
+		return "YOUR DITYYY";
+	} else if (userChoice == "rock" && computerChoice == "paper"){
+		return "YOU SUCK";
+	} else if (userChoice == "scissors" && computerChoice == "rock"){
+		return "YOU SUCK";
+	} else if (userChoice == "scissors" && computerChoice == "scissors"){
+		return "TIE";
+	} else if (userChoice == "scissors" && computerChoice == "paper"){
+		return "congrats, bro";
+	} else if (userChoice == "paper" && computerChoice == "paper"){
+		return "TIE";
+	} else if (userChoice == "paper" && computerChoice == "scissors"){
+		return "hah, nice try";
+	} else if (userChoice == "paper" && computerChoice == "rock"){
+		return "SWEEEET";
+	}else{
+		return "put in the just word stupid";
+	}
+}
+console.log(declareWinner(userChoice,computerChoice));
 
 });
